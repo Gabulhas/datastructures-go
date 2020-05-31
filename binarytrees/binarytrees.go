b05de3c5b3f19d820e457145dae78a14d4bad592
Guilherme Lopes <34821553+Gabulhas@users.noreply.github.com>
2020-05-31 21:36:05 +0100

    added BTGenerateRandomTree, BTSize and BTdisplayAllValues

--- binarytrees/binarytrees.go
+++ binarytrees/binarytrees.go
@@ -1,8 +1,8 @@
 package datastructures
 
 import (
-	"errors"
 	"fmt"
+	"math/rand"
 )
 
 //BTNodeInt SL = Single Link
@@ -11,10 +11,61 @@ type BTNodeInt struct {
 	NextR *BTNodeInt
 	NextL *BTNodeInt
 	Value int
-}	
-//BTMakeNode makes new 
+}
+
+//BTMakeNode makes new
 func BTMakeNode() *BTNodeInt {
 
-	return new(BTNodeInt) 
-	
+	return new(BTNodeInt)
+
+}
+
+//BTGenerateRandomTree generates random tree and returns root given a max and min number of childNodes, made for testing the functions implemented in this packages
+func BTGenerateRandomTree(maxValue int, maxLength int) *BTNodeInt {
+
+	head := BTMakeNode()
+	head.Value = randomInt(maxValue, 0)
+	generate(head, maxValue, maxLength)
+
+	return head
+}
+func generate(head *BTNodeInt, maxValue, maxLength int) {
+	if maxLength == 0 {
+		return
+	}
+
+	nodeR := BTMakeNode()
+	nodeR.Value = randomInt(maxValue, 0)
+
+	nodeL := BTMakeNode()
+	nodeL.Value = randomInt(maxValue, 0)
+	head.NextR = nodeR
+	head.NextL = nodeL
+	generate(nodeR, maxValue, randomInt(maxLength, 0))
+	generate(nodeL, maxValue, randomInt(maxLength, 0))
+}
+
+//BTSize Returns size of tree
+func BTSize(head *BTNodeInt) int {
+
+	if head == nil {
+		return 0
+	}
+	return 1 + BTSize(head.NextL) + BTSize(head.NextR)
+}
+
+//BTdisplayAllValues displays all values in a line. Just for testing purposes.
+func BTdisplayAllValues(head *BTNodeInt) {
+	if head == nil {
+		return
+	}
+
+	fmt.Printf("%d ", head.Value)
+	BTdisplayAllValues(head.NextR)
+	BTdisplayAllValues(head.NextL)
+}
+
+func randomInt(max, min int) int {
+
+	return rand.Intn(max-min+1) + min
 }
---
+++ btexample.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	dtbn "datastrucures_go/binarytrees"
+	"fmt"
+)
+
+func main() {
+
+	tree := dtbn.BTGenerateRandomTree(20, 4)
+	fmt.Println(dtbn.BTSize(tree))
+	dtbn.BTdisplayAllValues(tree)
+}
